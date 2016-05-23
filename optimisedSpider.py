480fcc1d51a376b5cd91d05be20b69252ce13bee
Anil Pediredla <pediredla.anil@gmail.com>
2016-05-23 03:54:29 -0500

    updated exception handling methods and email services

--- optimisedSpider.py
+++ optimisedSpider.py
@@ -1,15 +1,18 @@
-import gc, os, signal, sys
+import argparse
+import gc
 import random
-import time, argparse
+import smtplib
+import sys
+import time
 import unicodedata
+from smtplib import SMTPException
+
 import pymysql
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException
 from stem import Signal
 from stem.control import Controller
-import atexit, smtplib
-from smtplib import SMTPException
 
 controller = Controller.from_port(port=9051)
 
@@ -20,22 +23,22 @@ def releaseList(a):
 
 
 def SendMail(pidNumber, pidEnd):
-    sender = '******'
-    reciever = ['******']
+    sender = '****************'
+    reciever = ['***************']
     message = """
-              From: Anil Pediredla<*******>
-              To: Anil Pediredla<*******>
+              From: Anil Pediredla<***********>
+              To: Anil Pediredla<**********>
               Subject: Failed Job
 
-              From process running at host:<replace with host name> with Tor service<put the tor port here>
+              From process running at host: frenzy.ittc.ku.edu with Tor service 9050
               terminated with start pid as
-              """ + pidNumber + " and ending pid as" + pidEnd
+              """ + pidNumber + " and ending pid as " + pidEnd
     try:
         smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
         smtpObj.ehlo()
         smtpObj.starttls()
         smtpObj.ehlo()
-        smtpObj.login(sender, '******')
+        smtpObj.login(sender, '**********')
         smtpObj.sendmail(sender, reciever, message)
         smtpObj.close()
         sys.exit(0)
@@ -63,10 +66,10 @@ def normText(unicodeText):
 
 
 def ConnectDatabase():
-    conn = pymysql.connect(host='******',
+    conn = pymysql.connect(host='***********',
                            user='root',
-                           passwd='******',
                            db='linkedin',
+                           passwd='************',
                            charset='utf8mb4',
                            cursorclass=pymysql.cursors.DictCursor)
     return conn
@@ -89,17 +92,19 @@ def queryTable(newPerson):
 def reloadOnCaught(browser, pidNumberStart, pidEnd):
     response = True
     i = 0
+    browser.get("https://www.linkedin.com/in/jeffweiner08")
+    time.sleep(5)
     while response:
+        time.sleep(5)
         try:
-            if browser.find_element_by_id('first-name') or browser.find_element_by_id(
-                    'session_key-login') or browser.find_element_by_class_name('nav-link'):
-                print "linked in found us, chnage identity!!"
+            if (browser.find_element_by_id('first-name') or browser.find_element_by_id('session_key-login') or browser.find_element_by_class_name('nav-link')):
+                print "linked in found us, change identity!!"
                 newIdentity()
                 # browser.quit()
-                os.kill(browser.binary.process.pid, signal.SIGTERM)
+                browser.quit()
                 gc.collect()
                 i += 1
-                time.sleep(random.uniform(2, 5))
+                time.sleep(2)
                 newbrowser = webdriver.Firefox(firefox_profile=FirefoxProfileSettings())
                 # time.sleep(random.uniform(10, 20))
                 newbrowser.get("https://www.linkedin.com/in/jeffweiner08")
@@ -114,6 +119,7 @@ def reloadOnCaught(browser, pidNumberStart, pidEnd):
         except OSError as ose:
             SendMail(pidNumberStart, pidEnd)
     return browser
+
 
 
 def writeTofile(content):
@@ -137,12 +143,13 @@ def normText(unicodeText):
 def viewBot(browser, pidNumberStart, pidNumberEnd):
     # browser = reloadOnCaught(browser)
     conn = ConnectDatabase()
-    results = []
+    browsers = [browser]
+    i = 0
     trap = 0
     try:
         with conn.cursor() as cursor:
             cursor.execute(
-                'SELECT DISTINCT * FROM `pea` where (pid >' + pidNumberStart + ' and pid<=' + pidNumberEnd + ') GROUP BY pid')
+                'SELECT DISTINCT * FROM `pea` where (pid >=' + pidNumberStart + ' and pid<=' + pidNumberEnd + ') GROUP BY pid')
             results = cursor.fetchall()
 
         conn.commit()
@@ -164,6 +171,17 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
             time.sleep(random.uniform(5, 10))
 
             try:
+                if browser.find_element_by_class_name('nav-link') or browser.find_element_by_id(
+                        'session_key-login') or browser.find_element_by_id('first-name'):
+                    newBrowser = reloadOnCaught(browser,str(result['pid']), pidNumberEnd)
+                    #os.kill(browser.binary.process.pid, signal.SIGTERM)
+                    browser = newBrowser
+                    del newBrowser
+
+            except NoSuchElementException as noe0:
+                print "good to go!"
+
+            try:
                 firstNameElement = browser.find_element_by_id("firstName")
                 lastNameElement = browser.find_element_by_id("lastName")
                 firstNameElement.clear()
@@ -180,16 +198,20 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
                 if not browser.find_elements_by_class_name('fn'):
                     print "page has results checking headlines"
                     try:
-                        if browser.find_element_by_class_name('nav-links') or browser.find_elements_by_id('first-name') or browser.find_elements_by_id('session_key-login'):
-                            browser = reloadOnCaught(browser, str(result['pid']), pidNumberEnd)
+                        if browser.find_element_by_id('first-name') or browser.find_element_by_class_name(
+                                'nav-link') or browser.find_element_by_id('session_key-login'):
+                            newBrowser= reloadOnCaught(browser, str(result['pid']), pidNumberEnd)
+                            #os.kill(browser.binary.process.pid, signal.SIGTERM)
+                            browser = newBrowser
+                            del newBrowser
                             continue
-                    except NoSuchElementException as NoeS:
+                    except NoSuchElementException as noe1:
                         print "check the anchors!!"
                     elements = browser.find_elements_by_class_name('headline')
                     if elements:
-                        #print "here I am!"
+                        # print "here I am!"
                         for element in elements:
-                            #print "here in for loop"
+                            # print "here in for loop"
                             div = element.find_elements_by_xpath(
                                 "//*[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'" +
                                 result['school'] + "')]")
@@ -204,7 +226,7 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
                                 anchors = element.find_elements_by_xpath('..')
 
                                 for anchor in anchors:
-                                    #print "here in anchors"
+                                    # print "here in anchors"
                                     if (normText(result['school']).lower() in normText(anchor.text).lower()) or (
                                                 normText(result['affiliation']).lower() in normText(
                                                 anchor.text).lower()) or (
@@ -220,11 +242,18 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
                     # end of if elements
                     try:
                         browser.get(foundLink.get_attribute("href"))
-                    except AttributeError as AttrErr:
+                        if browser.find_element_by_id('first-name') or browser.find_element_by_id(
+                                'session_key-login') or browser.find_element_by_class_name('nav-link'):
+                            newBrowser = reloadOnCaught(browser,str(result['pid']), pidNumberEnd)
+                            #os.kill(browser.binary.process.pid, signal.SIGTERM)
+                            browser = newBrowser
+                            del newBrowser
+                    except AttributeError as attrErr:
                         print "writing everything to file"
                         writeTofile(browser.page_source)
-                        browser = reloadOnCaught(browser, str(result['pid']), pidNumberEnd)
                         continue
+                    except NoSuchElementException as noe2:
+                        print "good to go!!"
 
                 appendUrl(browser.current_url)
                 values.append(browser.find_element_by_xpath('//p[@data-section="headline"]').text)
@@ -244,23 +273,32 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
                             values.remove(degree)
                             values.remove(timeperiod)
 
-                            if edu.find_element_by_class_name("item-title") is list:
-                                for sch in edu.find_element_by_class_name("item-title"):
-                                    school = sch.text
-                            else:
-                                school = edu.find_element_by_class_name("item-title").text
+                            try:
 
-                            if edu.find_element_by_class_name("item-subtitle") is list:
-                                for deg in edu.find_element_by_class_name("item-subtitle"):
-                                    degree = deg.text
-                            else:
-                                degree = edu.find_element_by_class_name("item-subtitle").text
+                                if edu.find_element_by_class_name("item-title") is list:
+                                    for sch in edu.find_element_by_class_name("item-title"):
+                                        school = sch.text
+                                else:
+                                    school = edu.find_element_by_class_name("item-title").text
+                            except NoSuchElementException:
+                                school = ""
 
-                            if edu.find_element_by_class_name("date-range") is list:
-                                for ti in edu.find_element_by_class_name("date-range"):
-                                    timeperiod = ti.text
-                            else:
-                                timeperiod = edu.find_element_by_class_name("date-range").text
+                            try:
+                                if edu.find_element_by_class_name("item-subtitle") is list:
+                                    for deg in edu.find_element_by_class_name("item-subtitle"):
+                                        degree = deg.text
+                                else:
+                                    degree = edu.find_element_by_class_name("item-subtitle").text
+                            except NoSuchElementException:
+                                degree = ""
+                            try:
+                                if edu.find_element_by_class_name("date-range") is list:
+                                    for ti in edu.find_element_by_class_name("date-range"):
+                                        timeperiod = ti.text
+                                else:
+                                    timeperiod = edu.find_element_by_class_name("date-range").text
+                            except NoSuchElementException:
+                                timeperiod = ""
 
                             values.append(school)
                             values.append(degree)
@@ -270,25 +308,47 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
 
                             print tuple(values)
                     else:
-                        if education.find_element_by_class_name("item-title"):
+                        try:
+                            if education.find_element_by_class_name("item-title"):
+                                values.remove(school)
+                                values.remove(degree)
+                                values.remove(timeperiod)
+                                school = browser.find_element_by_class_name("item-title").text
+                                values.append(school)
+                                values.append(degree)
+                                values.append(timeperiod)
+                        except NoSuchElementException:
                             values.remove(school)
                             values.remove(degree)
                             values.remove(timeperiod)
-                            school = browser.find_element_by_class_name("item-title").text
+                            school = ""
                             values.append(school)
                             values.append(degree)
                             values.append(timeperiod)
-                        if education.find_element_by_class_name("item-subtitle"):
+
+                        try:
+                            if education.find_element_by_class_name("item-subtitle"):
+                                values.remove(degree)
+                                values.remove(timeperiod)
+                                degree = education.find_element_by_class_name("item-subtitle").text
+                                values.append(degree)
+                                values.append(timeperiod)
+                        except NoSuchElementException:
                             values.remove(degree)
                             values.remove(timeperiod)
-                            degree = education.find_element_by_class_name("item-subtitle").text
+                            degree = ""
                             values.append(degree)
                             values.append(timeperiod)
-                        if education.find_element_by_class_name("date-range"):
+                        try:
+                            if education.find_element_by_class_name("date-range"):
+                                values.remove(timeperiod)
+                                timeperiod = education.find_element_by_class_name("date-range").text
+                                values.append(timeperiod)
+                            queryTable(tuple(values))
+                        except NoSuchElementException:
                             values.remove(timeperiod)
-                            timeperiod = education.find_element_by_class_name("date-range").text
+                            timeperiod = ""
                             values.append(timeperiod)
-                        queryTable(tuple(values))
 
                         print tuple(values)
                 writeTofile(browser.page_source)
@@ -296,12 +356,14 @@ def viewBot(browser, pidNumberStart, pidNumberEnd):
                 print "stacktrace: %s" % Noe
                 if "firstName" in Noe.msg:
                     print "it's a trap, reload"
-                    browser = reloadOnCaught(browser, str(result['pid']), pidNumberEnd)
                     trap += 1
                     if trap == 15:
                         SendMail(str(result['pid']), pidNumberEnd)
+                    newBrowser = reloadOnCaught(browser, str(result['pid']), pidNumberEnd)
+                    #browser.quit()
+                    browser = newBrowser
                 continue
-            except StaleElementReferenceException as ele:
+            except StaleElementReferenceException as sle:
                 print "I am writing all to file"
                 writeTofile(browser.page_source)
                 # endof for loop
@@ -312,6 +374,7 @@ def main(start, end):
     browser = [webdriver.Firefox(firefox_profile=FirefoxProfileSettings())]
     browser[0].get("https://www.linkedin.com/in/jeffweiner08")
     i = 0
+    time.sleep(5)
     try:
         if browser[i].find_element_by_id('first-name') or browser[i].find_element_by_id('session_key-login'):
             browser.append(reloadOnCaught(browser[i], start, end))
@@ -332,9 +395,9 @@ if __name__ == "__main__":
     result = parser.parse_args()
     try:
         main(result.pid_start, result.pid_end)
-    except OSError as mom:
-        print "stacktrack is: %s" % mom
-        SendMail(result.pid_start, result.pid_end)
-    except Exception as e:
-        print "stackt is: %s" %e
-        SendMail(result.pid_start, result.pid_end)
+    except Exception as mom:
+        print "stacktrace is: %s" % mom
+        SendMail(str(result.pid_start), str(result.pid_end))
+    except OSError as e:
+        print "stacktrace is: %s" % e
+        SendMail(str(result.pid_start), str(result.pid_end))
